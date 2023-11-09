549adbcd36714e36c44360f1919701c4abadf7d7
kayquepiton <kayquepiton@gmail.com>
2023-11-09 17:06:58 -0300

    PrimeiroPrograma

---
+++ Semana1/MeuPrimeiroPrograma/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Hello, World!");
+
+#region Teste de tipos de Dados
+    int tipoInteiro;
+    Double tipoDouble;
+    string tipoString;
+    long tipoLong;
+
+    tipoInteiro = int.MaxValue;
+    tipoLong = tipoInteiro;
+    // tipoInteiro = tipoLong
+
+    tipoString = "100A";
+    tipoInteiro = tipoInteiro;
+
+    Console.WriteLine("O máximo de um tipo inteiro é: "+tipoInteiro);
+    Console.WriteLine("O máximo de um tipo long é: "+tipoLong);
+
+#endregion
