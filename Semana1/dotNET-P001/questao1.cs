d239f34a2e2f79f998adbfee1b4f116d14391b22
kayquepiton <kayquepiton@gmail.com>
2023-11-14 17:44:39 -0300

    Adicionando respostas para as 8 perguntas

---
+++ Semana1/dotNET-P001/questao1.cs
@@ -0,0 +1,16 @@
+#region 1. Configuração do Ambiente
+
+// Verificar a versão do .NET SDK instalado:
+dotnet --version
+
+// Atualizar o .NET SDK:
+dotnet --version               // Verificar a versão atual
+dotnet --list-sdks             // Listar as versões instaladas
+dotnet --list-runtimes         // Listar runtimes instalados
+dotnet --info                  // Exibir informações detalhadas
+dotnet --install-sdk <version>  // Instalar uma versão específica
+
+// Remover o .NET SDK:
+dotnet --uninstall-sdk <version>
+
+#endregion
---
+++ Semana1/dotNET-P001/questao3.cs
@@ -0,0 +1,9 @@
+#region 3. Conversão de Tipos de Dados
+
+double doubleValue = 15.75;
+int intValue = (int)doubleValue; // Conversão de double para int
+Console.WriteLine("Valor convertido: " + intValue);
+
+// Se a parte fracionária não puder ser convertida, a mesma será truncada.
+
+#endregion
