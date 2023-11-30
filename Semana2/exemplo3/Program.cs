29ab1252e35e704eae30c1029447973f69d5a61e
kayquepiton <kayquepiton@gmail.com>
2023-11-30 14:59:00 -0300

    Prova

--- Semana2/exemplo3/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// aqui temos um array, assunto da próxima aula
-string[] colecao= { "Item1", "Item2", "Item3", "Item4" };
-foreach (string item in colecao)
-{
-Console.WriteLine(item );
-}
--- Semana2/exercicioAula3/questao1/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿for (int i = 1; i <= 30; i++){
-    if(i % 3 == 0 || i % 4 == 0)
-        Console.WriteLine(i);
-}
